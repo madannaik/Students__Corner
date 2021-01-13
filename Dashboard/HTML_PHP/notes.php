38c8a44bc3984ddbd4ed5129efd5280f80e70bb4
Madan Naik <48375643+madannaik@users.noreply.github.com>
2021-01-13 16:13:19 +0530

    branch merged

--- Dashboard/HTML_PHP/notes.php
+++ Dashboard/HTML_PHP/notes.php
@@ -19,12 +19,79 @@ include('dashBoard.php');
 </head>
 
 <body>
+<div id="my-overlay" >
+    <!--TODO:Increase the width of pop up form-->
+    <!--TODO: Bring the popup form in the center-->
+    <!--    TODO:    change the color of the post button if possible-->
+    <!--        TODO: increase the text area for announcement-->
+    <div class="my-form-popup" id="myForm" >
+        <form action="announcementdb.php" class="form-container" method="POST">
+            <h1>New Announcement</h1>
+            <!--            <div class="form-group" style="width: 360px" >-->
+            <!--                <label for="exampleFormControlTextarea1" >Title</label>-->
+            <!--                <textarea class="form-control" id="exampleFormControlTextarea1" rows="1" name="title" required></textarea>-->
+            <!--            </div>-->
+            <div class="form-group" style="width: 410px" >
+                <label for="exampleFormControlTextarea1">Announcement</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" name="body" required></textarea>
+            </div>
+            <div class="form-group" style="width: 410px" >
+                <label for="exampleFormControlTextarea1">Link if any</label>
+                <input class="form-control" id="exampleFormControlTextarea1"  name="announcement_link" placeholder="null">
+            </div>
+            <div class="form-group" >
+                <label for="inputState">Choose Department</label>
+                <select id="inputState" class="form-control" name="department" required>
+                    <option selected>All</option>
+                    <option>Computer Science</option>
+                    <option>Electronic and Communication</option>
+                    <option>Electronics and Electrical</option>
+                    <option>Mechanical Engineering</option>
+                    <option>Information Science</option>
+                </select>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6" >
+                    <label for="inputState" >Semester</label>
+                    <select id="inputState" class="form-control" name="semester" required>
+                        <option selected>All</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+
+                    </select>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputState">Section</label>
+                    <select id="inputState" class="form-control" name="sections" required>
+                        <option selected>All</option>
+                        <option>A</option>
+                        <option>B</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button onclick="off()" type="button" class="btn btn-primary btn-sm bg-dark" style="background-color: #181717">Cancel</button>
+                </div>
+                <div class="col">
+                    <button type="submit" class="btn btn-primary btn-sm bg">Post</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 <div class="main-flex">
     <div class="courses-container">
         <div class="course">
             <div class="course-preview">
-                <h6>Department</h6>
-                <h2>Computer Science</h2>
+                <img src="../../assets/pc.svg" alt="svg" height="150" width="150" >
+
                 <a href="#">Last updated <i class="fas fa-chevron-right"></i></a>
                 <h6 style="font-size: 10px">2/10/2021</h6>
             </div>
@@ -35,9 +102,9 @@ include('dashBoard.php');
 <!--					6/9 Challenges-->
 <!--				</span>-->
 <!--                </div>-->
-                <h6>Chapter 4</h6>
-                <h2>Callbacks & Closures</h2>
-                <button class="btn">Continue</button>
+                <h6>Department</h6>
+                <h2>Computer Science</h2>
+                <button class="btn1">Continue</button>
                 <div class="selectdiv">
                     <label>
                         <select>
@@ -60,21 +127,20 @@ include('dashBoard.php');
     <div class="courses-container">
         <div class="course">
             <div class="course-preview">
+                <img src="../../assets/machine.svg" alt="svg" height="150" width="150" >
+                <a href="#">Last updated <i class="fas fa-chevron-right"></i></a>
+                <h6 style="font-size: 10px">2/10/2021</h6>
+            </div>
+            <div class="course-info">
+                <!--                <div class="progress-container">-->
+                <!--                    <div class="progress"></div>-->
+                <!--                    <span class="progress-text">-->
+                <!--					6/9 Challenges-->
+                <!--				</span>-->
+                <!--                </div>-->
                 <h6>Department</h6>
                 <h2>Mechanical</h2>
-                <a href="#">Last updated <i class="fas fa-chevron-right"></i></a>
-                <h6 style="font-size: 10px">2/10/2021</h6>
-            </div>
-            <div class="course-info">
-                <!--                <div class="progress-container">-->
-                <!--                    <div class="progress"></div>-->
-                <!--                    <span class="progress-text">-->
-                <!--					6/9 Challenges-->
-                <!--				</span>-->
-                <!--                </div>-->
-                <h6>Chapter 4</h6>
-                <h2>Callbacks & Closures</h2>
-                <button class="btn">Continue</button>
+                <button class="btn1">Continue</button>
                 <div class="selectdiv">
                     <label>
                         <select>
@@ -91,21 +157,20 @@ include('dashBoard.php');
     <div class="courses-container">
         <div class="course">
             <div class="course-preview">
+                <img src="../../assets/electric.svg" alt="svg" height="150" width="150" >
+                <a href="#">Last updated <i class="fas fa-chevron-right"></i></a>
+                <h6 style="font-size: 10px">2/10/2021</h6>
+            </div>
+            <div class="course-info">
+                <!--                <div class="progress-container">-->
+                <!--                    <div class="progress"></div>-->
+                <!--                    <span class="progress-text">-->
+                <!--					6/9 Challenges-->
+                <!--				</span>-->
+                <!--                </div>-->
                 <h6>Department</h6>
                 <h2>Electronics and Electrical</h2>
-                <a href="#">Last updated <i class="fas fa-chevron-right"></i></a>
-                <h6 style="font-size: 10px">2/10/2021</h6>
-            </div>
-            <div class="course-info">
-                <!--                <div class="progress-container">-->
-                <!--                    <div class="progress"></div>-->
-                <!--                    <span class="progress-text">-->
-                <!--					6/9 Challenges-->
-                <!--				</span>-->
-                <!--                </div>-->
-                <h6>Chapter 4</h6>
-                <h2>Callbacks & Closures</h2>
-                <button class="btn">Continue</button>
+                <button class="btn1">Continue</button>
                 <div class="selectdiv">
                     <label>
                         <select>
@@ -122,8 +187,7 @@ include('dashBoard.php');
     <div class="courses-container">
         <div class="course">
             <div class="course-preview">
-                <h6>Department</h6>
-                <h2>Information Science</h2>
+                <img src="../../assets/information.svg" alt="svg" height="150" width="150" >
                 <a href="#">Last updated <i class="fas fa-chevron-right"></i></a>
                 <h6 style="font-size: 10px">2/10/2021</h6>
             </div>
@@ -134,9 +198,9 @@ include('dashBoard.php');
                 <!--					6/9 Challenges-->
                 <!--				</span>-->
                 <!--                </div>-->
-                <h6>Chapter 4</h6>
-                <h2>Callbacks & Closures</h2>
-                <button class="btn">Continue</button>
+                <h6>Department</h6>
+                <h2>Information Science</h2>
+                <button class="btn1">Continue</button>
                 <div class="selectdiv">
                     <label>
                         <select id="new-select">
@@ -151,10 +215,30 @@ include('dashBoard.php');
         </div>
     </div>
 </div>
+<?php //if ($label == 'teacher'){ ?>
+    <!--this code is for Floating button for pop up form -->
+    <div class="my-pop-up-button" onclick="on()">
+        <i class="fas fa-plus" style="padding-top: 20px;"></i>
+    </div>
+<?php //} ?>
 
+<script>
+    function on() {
+        document.getElementById("my-overlay").style.display = "block";
+        // document.getElementById("my-body").style.overflow = "hidden";
+    }
+
+    function off() {
+        document.getElementById("my-overlay").style.display = "none";
+        // document.getElementById("my-body").style.overflow = "scroll";
+    }
+
+
+</script>
 <script>
     $(function () {
         let a = $('#new-select').value;
+        console.log(a);
         if (a === "Select semester"){
             $('.btn').attr("disabled");
         }
