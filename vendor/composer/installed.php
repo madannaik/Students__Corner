bf36e8580d558122f17aec40ccdeb87b959c0344
Madan Naik <48375643+madannaik@users.noreply.github.com>
2021-01-24 14:53:07 +0530

    upadated composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7516b56848cf02e84e65a38ef5a5f364f1e0ede8',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7516b56848cf02e84e65a38ef5a5f364f1e0ede8',
+    ),
+  ),
+);
